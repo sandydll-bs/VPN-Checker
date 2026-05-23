f81c90c5f25274c1a21cc2f1d824efa84dda7b50
sandy <sandyssacc@gmail.com>
2026-05-23 18:55:04 +0200

    Add files via upload

---
+++ VPNChecker.ps1
@@ -0,0 +1,30 @@
+Write-Host -ForegroundColor Magenta "VPN Checker | DS: @imsandy.dll"
+Write-Host ""
+
+$vpn = Get-NetIPInterface | Select-Object InterfaceAlias, ConnectionState
+
+foreach ($i in $vpn) {
+    $name = $i.InterfaceAlias.PadRight(40)
+    if ($i.InterfaceAlias -like "*VPN*") {
+        Write-Host "$name [VPN]" -ForegroundColor Red
+    }
+    elseif ($i.ConnectionState -eq "Connected") {
+        Write-Host "$name [CONNESSA]" -ForegroundColor Green
+    }
+    else {
+        Write-Host "$name [DISCONNESSA]" -ForegroundColor White
+    }
+}
+
+$requestJournal = Read-Host "Vuoi vedere se e' stata chiusa una VPN recentemente? (y/n)"
+
+if ($requestJournal -eq "y") {
+    Get-WinEvent -FilterHashtable @{
+        LogName = "System"
+        Id      = 4207
+    } -MaxEvents 20 |
+    Select-Object TimeCreated, Id
+}
+else {
+    Write-Host -ForegroundColor Magenta "Uscita dallo script..."
+}
